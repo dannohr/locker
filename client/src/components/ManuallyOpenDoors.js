3107d558bedc8c92d151772be90a0ca54ccc3aeb
Dan Nohr <danielnohr@gmail.com>
2018-01-31 11:54:43 -0600

    Start of Manually Open Locker Screen

--- client/src/components/ManuallyOpenDoors.js
+++ client/src/components/ManuallyOpenDoors.js
@@ -5,29 +5,58 @@ import RaisedButton from "material-ui/RaisedButton";
 
 let style = { margin: 20 };
 
-function callApi() {
-  console.log("hi");
-}
+let lockers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 class ManuallyOpenDoors extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       doorStatus: [],
-      colors: []
+      colors: [],
+      singleMode: false,
+      selectedLocker: []
     };
+  }
+
+  updateColors(res) {
+    let doorStatus = res.data[0].doorOpen;
+    let colors = [];
+    for (var i = 0; i < doorStatus.length; i++) {
+      colors.push(doorStatus[i] ? "pink" : "#8de8aa");
+    }
+    this.setState({ doorStatus, colors });
+  }
+
+  handleToggleMode() {
+    this.setState({
+      singleMode: !this.state.singleMode
+    });
+  }
+
+  callApi(num) {
+    console.log("Opening Door", num);
+    axios
+      .post(`http://localhost:3001/api/postOpenLock`, {
+        lock: [num],
+        attempts: 3
+      })
+      .then(res => {
+        this.updateColors(res);
+      });
+  }
+
+  setLocker(num) {
+    console.log("Setting Locker", num);
+    let selectedLocker = this.state.selectedLocker;
+    selectedLocker.push(num);
+    this.setState({ selectedLocker });
+
+    console.log(this.state);
   }
 
   componentDidMount() {
     axios.get(`http://localhost:3001/api/getAllInputStatus`).then(res => {
-      let doorStatus = res.data[0].doorOpen;
-      let colors = [];
-      for (var i = 0; i < doorStatus.length; i++) {
-        colors.push(doorStatus[i] ? "pink" : "green");
-      }
-      this.setState({ doorStatus, colors });
-      console.log(this.state);
+      this.updateColors(res);
     });
   }
 
@@ -35,69 +64,55 @@ class ManuallyOpenDoors extends React.Component {
     return (
       <MuiThemeProvider>
         <div>
-          <h1> Manually Open Lockers </h1>
+          <h2> Manually Open Lockers </h2>
+          <hr />
           <div>
+            {this.state.singleMode
+              ? "Open Single Doors"
+              : "Open Multiple Doors"}
+
             <RaisedButton
-              label="1"
+              label="Toggle"
               style={style}
-              backgroundColor={this.state.colors[0]}
-              onClick={() => callApi()}
+              onClick={e => {
+                this.handleToggleMode();
+              }}
             />
-            <RaisedButton
-              label="2"
-              style={style}
-              backgroundColor={this.state.colors[1]}
-            />
-            <RaisedButton
-              label="3"
-              style={style}
-              backgroundColor={this.state.colors[2]}
-            />
-            <RaisedButton
-              label="4"
-              style={style}
-              backgroundColor={this.state.colors[3]}
-            />
-            <RaisedButton
-              label="5"
-              style={style}
-              backgroundColor={this.state.colors[4]}
-            />
-            <RaisedButton
-              label="6"
-              style={style}
-              backgroundColor={this.state.colors[5]}
-            />
-            <RaisedButton
-              label="7"
-              style={style}
-              backgroundColor={this.state.colors[6]}
-            />
-            <RaisedButton
-              label="8"
-              style={style}
-              backgroundColor={this.state.colors[8]}
-            />
+            <hr />
           </div>
+
+          {!this.state.singleMode ? (
+            <RaisedButton
+              label="Open Selected"
+              style={style}
+              onClick={e => {
+                this.handleToggleMode();
+              }}
+            />
+          ) : null}
+
           <div>
-            <RaisedButton label="9" style={style} />
-            <RaisedButton label="10" style={style} />
-            <RaisedButton label="11" style={style} />
-            <RaisedButton label="12" style={style} />
-            <RaisedButton label="13" style={style} />
-            <RaisedButton label="14" style={style} />
-            <RaisedButton label="15" style={style} />
-            <RaisedButton label="16" style={style} />
-          </div>
-          <div>
-            <RaisedButton label="17" style={style} />
-            <RaisedButton label="18" style={style} />
-            <RaisedButton label="19" style={style} />
-            <RaisedButton label="20" style={style} />
-            <RaisedButton label="21" style={style} />
-            <RaisedButton label="22" style={style} />
-            <RaisedButton label="23" style={style} />
-            <RaisedButton label="24" style={style} />
+            {lockers.map(i => {
+              return (
+                <RaisedButton
+                  className={
+                    this.state.singleMode === false &&
+                    this.state.selectedLocker.includes(i)
+                      ? "selected-locker-button"
+                      : "button"
+                  }
+                  key={i}
+                  label={i}
+                  style={style}
+                  backgroundColor={this.state.colors[i - 1]}
+                  onClick={() =>
+                    this.state.singleMode === true
+                      ? this.callApi(i - 1)
+                      : this.setLocker(i)
+                  }
+                />
+              );
+            })}
           </div>
         </div>
       </MuiThemeProvider>
--- client/src/components/WalkupScreen.js
+++ client/src/components/WalkupScreen.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import RaisedButton from "material-ui/RaisedButton";
 const style = {
   margin: 12
 };
 
-const WalkupScreen = () => (
-  <div>
-    <MuiThemeProvider>
-      <RaisedButton label="Drop Off Package" style={style} />
-      <RaisedButton label="Retrieve Package" style={style} />
-    </MuiThemeProvider>
-  </div>
-);
+class WalkupScreen extends React.Component {
+  nextPath(path) {
+    this.props.history.push(path);
+  }
 
-export default WalkupScreen;
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <RaisedButton label="Drop Off Package" style={style} />
+          <RaisedButton label="Retrieve Package" style={style} />
+          <RaisedButton
+            label="Manually Open Doors"
+            style={style}
+            onClick={() => this.nextPath("/manualdooropen")}
+          />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default withRouter(WalkupScreen);
